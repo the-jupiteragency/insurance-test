df5f6e241cd185e79c4ed827a0a92a2fb2e9eba4
the-jupiteragency <thejupiteragency@gmail.com>
2026-05-04 19:27:15 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import { LanguageProvider } from "@/app/LanguageContext";
+import Navbar from "@/app/components/Navbar";
+import { Cairo } from "next/font/google";
+import "./globals.css";
+import type { Metadata } from "next";
+import ContactSection from "@/app/components/ContactSection";
+
+const cairo = Cairo({
+  subsets: ["latin", "arabic"],
+  variable: "--font-cairo",
+});
+
+export const metadata: Metadata = {
+  title: "Sky Insurance",
+  description: "Preserving Prosperity, Providing Peace of Mind",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${cairo.className} antialiased`}>
+        <LanguageProvider>
+          <Navbar />
+          <main>{children}</main>
+          <ContactSection />
+        </LanguageProvider>
+      </body>
+    </html>
+  );
+}
